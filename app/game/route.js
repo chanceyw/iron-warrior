26008b6a61b1e2eed2f2368bbe283edf83b81ef9
Chancey Ward <chancey_ward@yahoo.com>
2016-01-05 13:47:07 -0600

    linked up models to main raw header

--- app/game/route.js
+++ app/game/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('character', {
+      name: 'Mr. David',
+      cash: 100,
+      experience: 50,
+      characterClass: 'Warrior',
+      damage: 10,
+    });
+  },
 });
--- app/models/character.js
+++ app/models/character.js
@@ -1,17 +1,45 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
+let startingHealth = 60;
+let healthMultiplier = 10;
+let damageMultiplier = 10;
+let startingBaseAttack = 10;
 let levelOneXp = 10;
 
-function getLevel(x) {
+function getXpFromLevel(level) {
+  return Math.pow(2, level - 2) * levelOneXp;
+}
+
+function getLevelFromXp(x) {
   return (Math.log(x / levelOneXp) / Math.log(2)) + 1;
 }
 
 export default DS.Model.extend({
-  class: DS.attr('string', {defaultValue: 'warrior'}),
-  healthPoints: DS.attr('number', {defaultValue: '60'}),
-  attackDamage: DS.attr('number', {defaultValue:  '10'}),
-  experience: DS.attr('number', {defaultValue: '0'}),
+  name: DS.attr('string', {defaultValue: 'Bob'}),
+  characterClass: DS.attr('string', {defaultValue: 'Warrior'}),
+  damage: DS.attr('number', {defaultValue: 0}),
+  cash: DS.attr('number', {defaultValue: 0}),
+  experience: DS.attr('number', {defaultValue: 0}),
+
+  baseAttack: Ember.computed('level', function() {
+    let level = this.get('level');
+
+    return level * damageMultiplier + startingBaseAttack;
+  }),
+
+  maxHealthPoints: Ember.computed('level', function() {
+    let level = this.get('level');
+
+    return level * healthMultiplier + startingHealth;
+  }),
+
+  currentHealthPoints: Ember.computed('maxHealthPoints', 'damage', function() {
+    let {maxHealthPoints, damage} = this.getProperties('maxHealthPoints', 'damage');
+
+    return maxHealthPoints - damage;
+  }),
+
   level: Ember.computed('experience', function() {
     let experience = this.get('experience');
 
@@ -19,6 +47,14 @@ export default DS.Model.extend({
       return 1;
     }
 
-    return Math.floor(getLevel(experience)) + 1;
+    return Math.floor(getLevelFromXp(experience)) + 1;
   }),
+
+  nextLevel: Ember.computed('level', function() {
+    return this.get('level') + 1;
+  }),
+
+  nextLevelXp: Ember.computed('nextLevel', function() {
+    return getXpFromLevel(this.get('nextLevel'));
+  })
 });
