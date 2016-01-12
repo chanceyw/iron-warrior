acf6a2870506f87d841872646389dfe9eb29eedb
Chancey Ward <chancey_ward@yahoo.com>
2016-01-12 14:09:18 -0600

    set up styles for buttons got selected item to work

--- app/components/battle-screen/component.js
+++ app/components/battle-screen/component.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['battle-container'],
-
+  currentWeapon: null,
   enemy: null,
   currentUser: Ember.inject.service('current-user'),
+  actions: {
+    select(item) {
+      this.set('currentWeapon', item);
+    },
+  },
 });
